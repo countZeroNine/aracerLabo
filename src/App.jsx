46e32a1cf029a9af649722d27177609392c832ad
countZeroNine <zoo1984@gmail.com>
2026-05-16 01:45:01 +0900

    App.jsx を更新

--- src/App.jsx
+++ src/App.jsx
@@ -158,7 +158,7 @@ export default function App() {
                   style={{ color: COLORS.textDim, border: `1px solid ${COLORS.border}` }}>
             ?
           </button>
-          <input ref={fileInputRef} type="file" accept=".loga,text/plain"
+          <input ref={fileInputRef} type="file" accept="＊"
                  className="hidden"
                  onChange={(e) => { if (e.target.files?.[0]) handleFile(e.target.files[0]); }} />
           <button onClick={() => fileInputRef.current?.click()}
